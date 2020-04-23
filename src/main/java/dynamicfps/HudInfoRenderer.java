9fccb1c7d6d700fb2b927894193161b7cb4ecc59
Siphalor <info@siphalor.de>
2020-04-23 02:33:00 +0200

    Update to 20w17a

--- src/main/java/dynamicfps/HudInfoRenderer.java
+++ src/main/java/dynamicfps/HudInfoRenderer.java
@@ -3,28 +3,25 @@ package dynamicfps;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
+import net.minecraft.client.resource.language.I18n;
+import net.minecraft.client.util.math.MatrixStack;
 
 public final class HudInfoRenderer implements HudRenderCallback {
 	@Override
-	public void onHudRender(float delta) {
+	public void onHudRender(MatrixStack matrixStack, float delta) {
 		if (DynamicFPSMod.isForcingLowFPS()) {
 			MinecraftClient client = MinecraftClient.getInstance();
 			TextRenderer textRenderer = client.inGameHud.getFontRenderer();
 			
-			String textToRender = text("reducing").asFormattedString();
+			String textToRender = I18n.translate("gui." + DynamicFPSMod.MOD_ID + ".hud.reducing");
 			int windowWidth = client.getWindow().getScaledWidth();
 			int stringWidth = textRenderer.getStringWidth(textToRender);
 			textRenderer.drawWithShadow(
+				matrixStack,
 				textToRender,
 				(windowWidth - stringWidth) / 2f, 32,
 				0xffffff
 			);
 		}
-	}
-	
-	private static Text text(String path, Object... args) {
-		return new TranslatableText("gui." + DynamicFPSMod.MOD_ID + ".hud." + path, args);
 	}
 }
