1bf68a5c10bd5c57f08b3027dfd799f786e600e3
LostLuma <lilly@lostluma.net>
2023-06-08 11:26:37 +0200

    Update to 1.20.0

--- src/main/java/dynamicfps/HudInfoRenderer.java
+++ src/main/java/dynamicfps/HudInfoRenderer.java
@@ -3,32 +3,42 @@ package dynamicfps;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.font.TextRenderer.TextLayerType;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.text.Text;
 
 import static dynamicfps.util.Localization.localized;
 
+import org.joml.Matrix4f;
+
 public final class HudInfoRenderer implements HudRenderCallback {
 	@Override
-	public void onHudRender(MatrixStack matrices, float delta) {
+	public void onHudRender(GuiGraphics drawContext, float tickDelta) {
 		if (DynamicFPSMod.isDisabled()) {
-			drawCenteredText(matrices, localized("gui", "hud.disabled"), 32);
+			drawCenteredText(drawContext, localized("gui", "hud.disabled"), 32);
 		} else if (DynamicFPSMod.isForcingLowFPS()) {
-			drawCenteredText(matrices, localized("gui", "hud.reducing"), 32);
+			drawCenteredText(drawContext, localized("gui", "hud.reducing"), 32);
 		}
 	}
-	
-	private void drawCenteredText(MatrixStack matrices, Text text, float y) {
+
+	private void drawCenteredText(GuiGraphics drawContext, Text text, float y) {
 		MinecraftClient client = MinecraftClient.getInstance();
 		TextRenderer textRenderer = client.inGameHud.getTextRenderer();
-		
+
 		int windowWidth = client.getWindow().getScaledWidth();
 		int stringWidth = textRenderer.getWidth(text);
-		textRenderer.drawWithShadow(
-			matrices,
+
+		textRenderer.draw(
 			text,
-			(windowWidth - stringWidth) / 2f, y,
-			0xffffff
+			(windowWidth - stringWidth) / 2f,
+			y,
+			0xFFFFFFFF,
+			true,
+			new Matrix4f(),
+			drawContext.getVertexConsumers(),
+			TextLayerType.NORMAL,
+			0,
+			0xFFFFFF
 		);
 	}
 }
