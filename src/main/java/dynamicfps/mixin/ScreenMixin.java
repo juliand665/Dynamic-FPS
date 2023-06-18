ec38881526c122430600bc8165d19c6ff39ff70a
LostLuma <lilly@lostluma.net>
2023-06-18 15:30:40 +0200

    Allow developers to opt into screen optimization via mod metadata

--- src/main/java/dynamicfps/mixin/ScreenMixin.java
+++ src/main/java/dynamicfps/mixin/ScreenMixin.java
@@ -7,27 +7,34 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import dynamicfps.util.DynamicFPSScreen;
-import net.minecraft.client.gui.screen.DownloadingTerrainScreen;
+import dynamicfps.util.ScreenOptimizationCompat;
 import net.minecraft.client.gui.screen.Screen;
 
 @Mixin(Screen.class)
 public class ScreenMixin implements DynamicFPSScreen {
 	@Unique
-	private boolean dynamicfps$rendersBackground = false;
+	private boolean dynamicfps$canOptimize = false;
+
+	@Unique
+	private boolean dynamicfps$hasOptedOut = false;
 
 	@Override
 	public boolean dynamicfps$rendersBackground() {
-		return dynamicfps$rendersBackground;
+		return dynamicfps$canOptimize;
+	}
+
+	@Inject(method = "init", at = @At("HEAD"))
+	private void onInit(CallbackInfo callbackInfo) {
+		String name = this.getClass().getName();
+
+		this.dynamicfps$canOptimize = ScreenOptimizationCompat.isOptedIn(name);
+		this.dynamicfps$hasOptedOut = ScreenOptimizationCompat.isOptedOut(name);
 	}
 
 	@Inject(method = "renderBackgroundTexture", at = @At("HEAD"))
 	private void onRenderBackgroundTexture(CallbackInfo callbackInfo) {
-		// This screen is not compatible with this optimization,
-		// As it waits for chunks to be done rendering to close.
-		if ((Screen)(Object)this instanceof DownloadingTerrainScreen) {
-			return;
+		if (!this.dynamicfps$hasOptedOut) {
+			this.dynamicfps$canOptimize = true; // Signal to apply optimizations on next frame
 		}
-
-		this.dynamicfps$rendersBackground = true; // Signal to apply optimizations on next frame
 	}
 }
