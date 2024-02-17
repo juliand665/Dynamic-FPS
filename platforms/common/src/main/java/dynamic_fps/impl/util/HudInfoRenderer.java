2e0091d2d92b3be7b4a5271966194aa78a884304
LostLuma <lilly@lostluma.net>
2024-02-17 19:48:48 +0100

    1.19.3

--- platforms/common/src/main/java/dynamic_fps/impl/util/HudInfoRenderer.java
+++ platforms/common/src/main/java/dynamic_fps/impl/util/HudInfoRenderer.java
@@ -2,44 +2,37 @@ package dynamic_fps.impl.util;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.Font.DisplayMode;
 import net.minecraft.network.chat.Component;
 
 import static dynamic_fps.impl.util.Localization.localized;
 
-import org.joml.Matrix4f;
+import com.mojang.blaze3d.vertex.PoseStack;
 
 import dynamic_fps.impl.DynamicFPSMod;
 
 public final class HudInfoRenderer {
 	private static final Minecraft minecraft = Minecraft.getInstance();
 
-	public static void renderInfo(GuiGraphics guiGraphics) {
+	public static void renderInfo(PoseStack poseStack) {
 		if (DynamicFPSMod.disabledByUser()) {
-			drawCenteredText(guiGraphics, localized("gui", "hud.disabled"), 32);
+			drawCenteredText(poseStack, localized("gui", "hud.disabled"), 32);
 		} else if (DynamicFPSMod.isForcingLowFPS()) {
-			drawCenteredText(guiGraphics, localized("gui", "hud.reducing"), 32);
+			drawCenteredText(poseStack, localized("gui", "hud.reducing"), 32);
 		}
 	}
 
-	private static void drawCenteredText(GuiGraphics guiGraphics, Component component, float y) {
+	private static void drawCenteredText(PoseStack poseStack, Component component, float y) {
 		Font fontRenderer = minecraft.gui.getFont();
 
 		int stringWidth = fontRenderer.width(component);
 		int windowWidth = minecraft.getWindow().getGuiScaledWidth();
 
-		fontRenderer.drawInBatch(
+		fontRenderer.drawShadow(
+			poseStack,
 			component,
 			(windowWidth - stringWidth) / 2f,
 			y,
-			0xFFFFFFFF,
-			true,
-			new Matrix4f(),
-			guiGraphics.bufferSource(),
-			DisplayMode.NORMAL,
-			0,
-			255
+			0xFFFFFFFF
 		);
 	}
 }
