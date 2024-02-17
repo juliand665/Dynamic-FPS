b6ca2e7c06bae399cc4a9cf306ada63199641b1a
LostLuma <lilly@lostluma.net>
2024-02-17 19:28:02 +0100

    Use cross-platform mixin to render HUD overlay
    Turns out this *can* work after all, if I don't use MixinExtras?

--- platforms/common/src/main/java/dynamic_fps/impl/mixin/GuiMixin.java
+++ platforms/common/src/main/java/dynamic_fps/impl/mixin/GuiMixin.java
@@ -1,5 +1,6 @@
 package dynamic_fps.impl.mixin;
 
+import dynamic_fps.impl.util.HudInfoRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -7,6 +8,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import dynamic_fps.impl.DynamicFPSMod;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphics;
 
 @Mixin(Gui.class)
 public class GuiMixin {
@@ -18,5 +20,19 @@ public class GuiMixin {
 		if (!DynamicFPSMod.shouldShowLevels()) {
 			callbackInfo.cancel();
 		}
+	}
+
+	/**
+	 * Render info on whether Dynamic FPS is disabled or always reducing the user's FPS.
+	 */
+	@Inject(
+		method = "render",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/gui/Gui;renderSavingIndicator(Lnet/minecraft/client/gui/GuiGraphics;)V"
+		)
+	)
+	private void render(GuiGraphics guiGraphics, float f, CallbackInfo callbackInfo) {
+		HudInfoRenderer.renderInfo(guiGraphics);
 	}
 }
