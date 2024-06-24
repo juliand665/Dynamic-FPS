080206cf084c8c8304a065f9db6cc3d80392d823
LostLuma <lilly@lostluma.net>
2024-06-24 21:35:27 +0200

    Minecraft 1.16.5 backport

--- platforms/neoforge/src/main/java/net/lostluma/dynamic_fps/impl/neoforge/service/NeoForgeModCompat.java
+++ platforms/neoforge/src/main/java/net/lostluma/dynamic_fps/impl/neoforge/service/NeoForgeModCompat.java
@@ -2,13 +2,19 @@ package net.lostluma.dynamic_fps.impl.neoforge.service;
 
 import com.electronwill.nightconfig.core.CommentedConfig;
 import dynamic_fps.impl.service.ModCompat;
+import net.minecraft.client.gui.screens.ReceivingLevelScreen;
 import net.neoforged.fml.ModList;
 import net.neoforged.neoforgespi.language.IModInfo;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class NeoForgeModCompat implements ModCompat {
 	private static boolean disableOverlayOptimization = false;
+	private static final Set<String> optedOutScreens = new HashSet<>();
 
 	static {
+		optedOutScreens.add(ReceivingLevelScreen.class.getCanonicalName());
 		ModList.get().getMods().forEach(NeoForgeModCompat::parseModMetadata);
 	}
 
@@ -20,6 +26,16 @@ public class NeoForgeModCompat implements ModCompat {
 	@Override
 	public boolean disableOverlayOptimization() {
 		return disableOverlayOptimization || ModList.get().isLoaded("rrls");
+	}
+
+	@Override
+	public Set<String> getOptedInScreens() {
+		return Set.of();
+	}
+
+	@Override
+	public Set<String> getOptedOutScreens() {
+		return optedOutScreens;
 	}
 
 	private static void parseModMetadata(IModInfo modInfo) {
