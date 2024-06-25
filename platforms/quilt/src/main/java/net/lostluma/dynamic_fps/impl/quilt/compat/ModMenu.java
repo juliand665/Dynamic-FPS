2cfcfe484bf2541f6898cdae24acbe77fd805650
LostLuma <lilly@lostluma.net>
2024-06-25 15:04:55 +0200

    Fix Quilt ModMenu impl not using new fallback config screen

--- platforms/quilt/src/main/java/net/lostluma/dynamic_fps/impl/quilt/compat/ModMenu.java
+++ platforms/quilt/src/main/java/net/lostluma/dynamic_fps/impl/quilt/compat/ModMenu.java
@@ -2,18 +2,12 @@ package net.lostluma.dynamic_fps.impl.quilt.compat;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import dynamic_fps.impl.compat.ClothConfig;
-import org.quiltmc.loader.api.QuiltLoader;
+
+import dynamic_fps.impl.DynamicFPSMod;
 
 public class ModMenu implements ModMenuApi {
-	private static final boolean CLOTH_CONFIG = QuiltLoader.isModLoaded("cloth-config2");
-
 	@Override
 	public ConfigScreenFactory<?> getModConfigScreenFactory() {
-		if (!CLOTH_CONFIG) {
-			return parent -> null;
-		} else {
-			return ClothConfig::genConfigScreen;
-		}
+		return DynamicFPSMod::getConfigScreen;
 	}
 }
