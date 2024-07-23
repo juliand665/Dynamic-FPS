d79b47b71309f59430eb812f69bb36ffb7f0eddd
LostLuma <lilly@lostluma.net>
2024-07-23 15:30:34 +0200

    Explicitly set Kotlin JVM version in build-logic
    Fixes a pretty loud deprecation warning when building.

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -26,3 +26,7 @@ dependencies {
     // https://github.com/gradle/gradle/issues/15383
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
 }
+
+kotlin {
+    jvmToolchain(21)
+}
