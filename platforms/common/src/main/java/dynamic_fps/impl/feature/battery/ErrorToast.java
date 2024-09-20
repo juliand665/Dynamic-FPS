d7933dd20a0586fa74f50dd2bc4bba5418a541d1
LostLuma <lilly@lostluma.net>
2024-09-20 10:27:52 +0200

    Minecraft 1.20.0 backport
    Signed-off-by: Lilly Rose Berner <lilly@lostluma.net>

--- platforms/common/src/main/java/dynamic_fps/impl/feature/battery/ErrorToast.java
+++ platforms/common/src/main/java/dynamic_fps/impl/feature/battery/ErrorToast.java
@@ -17,6 +17,6 @@ public class ErrorToast extends BaseToast {
 	 */
 	public static void queueToast(Component description) {
 		ErrorToast toast = new ErrorToast(description);
-		Minecraft.getInstance().getToastManager().addToast(toast);
+		Minecraft.getInstance().getToasts().addToast(toast);
 	}
 }
