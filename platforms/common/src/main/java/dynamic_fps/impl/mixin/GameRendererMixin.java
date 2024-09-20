da1773f9580fade8e2c1e1882e0cf61244a8ae06
LostLuma <lilly@lostluma.net>
2024-09-20 10:27:52 +0200

    Minecraft 1.19.0 backport
    Signed-off-by: Lilly Rose Berner <lilly@lostluma.net>

--- platforms/common/src/main/java/dynamic_fps/impl/mixin/GameRendererMixin.java
+++ platforms/common/src/main/java/dynamic_fps/impl/mixin/GameRendererMixin.java
@@ -28,7 +28,7 @@ public class GameRendererMixin {
 		)
 	)
 	private boolean skipRendering(boolean original) {
-		return original || !DynamicFPSMod.renderedCurrentFrame();
+		return original || !DynamicFPSMod.checkForRender();
 	}
 
 	/**
