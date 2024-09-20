038655d20409e95af11393413ec7e5cc97393d83
LostLuma <lilly@lostluma.net>
2024-09-20 10:27:52 +0200

    Minecraft 1.17.0 backport
    Signed-off-by: Lilly Rose Berner <lilly@lostluma.net>

--- platforms/common/src/main/java/dynamic_fps/impl/feature/battery/BaseToast.java
+++ platforms/common/src/main/java/dynamic_fps/impl/feature/battery/BaseToast.java
@@ -1,11 +1,12 @@
 package dynamic_fps.impl.feature.battery;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
 import dynamic_fps.impl.util.ResourceLocations;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Font;
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.gui.components.toasts.Toast;
-import net.minecraft.client.gui.components.toasts.ToastManager;
+import net.minecraft.client.gui.components.toasts.ToastComponent;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -14,7 +15,6 @@ import org.jetbrains.annotations.Nullable;
 
 public class BaseToast implements Toast {
 	private long firstRender;
-	private Visibility visibility;
 
 	protected Component title;
 	protected Component description;
@@ -28,47 +28,33 @@ public class BaseToast implements Toast {
 		this.description = description;
 
 		this.icon = icon;
-
-		this.visibility = Visibility.SHOW;
 	}
 
 	@Override
-	public @NotNull Visibility getWantedVisibility() {
-		return this.visibility;
-	}
-
-	@Override
-	public void update(ToastManager toastManager, long currentTime) {
-		if (this.firstRender == 0) {
-			return;
-		}
-
-		if (currentTime - this.firstRender >= 5000.0 * toastManager.getNotificationDisplayTimeMultiplier()) {
-			this.visibility = Visibility.HIDE;
-		}
-	}
-
-	@Override
-	public void render(GuiGraphics graphics, Font font, long currentTime) {
+	public @NotNull Visibility render(PoseStack poseStack, ToastComponent toastComponent, long currentTime) {
 		if (this.firstRender == 0) {
 			this.onFirstRender();
 			this.firstRender = currentTime;
 		}
 
-		// type, resource, x, y, ?, ?, width, height, width, height
-		graphics.blit(RenderType::guiTextured, BACKGROUND_IMAGE, 0, 0, 0.0f, 0, this.width(), this.height(), this.width(), this.height());
+		RenderSystem.setShaderTexture(0, BACKGROUND_IMAGE);
+		GuiComponent.blit(poseStack, 0, 0, 0.0f, 0.0f, 160, 32, 160, 32);
 
 		int x = 8;
 
 		if (this.icon != null) {
 			x += 22;
 
-			graphics.blit(RenderType::guiTextured, MOD_ICON, 2, 2, 0.0f, 0, 8, 8, 8, 8);
-			graphics.blit(RenderType::guiTextured, this.icon, 8, 8, 0.0f, 0, 16, 16, 16, 16);
+			RenderSystem.setShaderTexture(0, MOD_ICON);
+			GuiComponent.blit(poseStack, 2, 2, 0.0f, 0.0f, 8, 8, 8, 8);
+			RenderSystem.setShaderTexture(0, this.icon);
+			GuiComponent.blit(poseStack, 8, 8, 0.0f, 0.0f, 16, 16, 16, 16);
 		}
 
-		graphics.drawString(Minecraft.getInstance().font, this.title, x, 7, 0x5f3315, false);
-		graphics.drawString(Minecraft.getInstance().font, this.description, x, 18, -16777216, false);
+		Minecraft.getInstance().font.draw(poseStack, this.title, x, 7, 0x5f3315);
+		Minecraft.getInstance().font.draw(poseStack, this.description, x, 18, -16777216);
+
+		return currentTime - this.firstRender >= 5000.0 ? Toast.Visibility.HIDE : Toast.Visibility.SHOW;
 	}
 
 	public void onFirstRender() {}
