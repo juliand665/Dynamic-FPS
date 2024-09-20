23c5ba806e8c1d18965bb8b62328b14f55a18b75
LostLuma <lilly@lostluma.net>
2024-09-20 10:27:52 +0200

    Minecraft 1.21.0 backport

--- platforms/common/src/main/java/dynamic_fps/impl/mixin/ToastComponentMixin.java
+++ platforms/common/src/main/java/dynamic_fps/impl/mixin/ToastComponentMixin.java
@@ -1,17 +1,17 @@
 package dynamic_fps.impl.mixin;
 
-import net.minecraft.client.gui.components.toasts.ToastManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import dynamic_fps.impl.DynamicFPSMod;
+import net.minecraft.client.gui.components.toasts.ToastComponent;
 
-@Mixin(ToastManager.class)
-public class ToastManagerMixin {
-	@Inject(method = "freeSlotCount", at = @At("HEAD"), cancellable = true)
-	private void freeSlotCount(CallbackInfoReturnable<Integer> callbackInfo) {
+@Mixin(ToastComponent.class)
+public class ToastComponentMixin {
+	@Inject(method = "freeSlots", at = @At("HEAD"), cancellable = true)
+	private void hasFreeSlots(CallbackInfoReturnable<Integer> callbackInfo) {
 		if (!DynamicFPSMod.shouldShowToasts()) {
 			callbackInfo.setReturnValue(0);
 		}
