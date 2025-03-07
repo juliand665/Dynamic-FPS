84be5c9ebbfb9f8573c7ea8d0c0c5e0a5c3f93e2
LostLuma <lilly@lostluma.net>
2025-03-07 11:25:55 +0100

    Fix no FPS limit being applied when using a non-default idle condition
    Signed-off-by: Lilly Rose Berner <lilly@lostluma.net>

--- platforms/common/src/main/java/dynamic_fps/impl/Constants.java
+++ platforms/common/src/main/java/dynamic_fps/impl/Constants.java
@@ -13,6 +13,8 @@ public class Constants {
 	public static final int MIN_FRAME_RATE_LIMIT = 15;
 	// Minecraft considers limits >=260 as unlimited
 	public static final int NO_FRAME_RATE_LIMIT = 260;
+	// Default frame rate limit on all title screens
+	public static final int TITLE_FRAME_RATE_LIMIT = 60;
 
 	// The Cloth Config mod ID has changed a few times
 	public static final String[] CLOTH_CONFIG_ID = new String[] { "cloth-config", "cloth_config", "cloth-config2" };
